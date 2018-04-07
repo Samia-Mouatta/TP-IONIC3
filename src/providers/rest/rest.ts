af62ed5d91469cbfa826b28232ffce06576bb4db
Samia-Mouatta <samia.mouatta@gmail.com>
2018-04-07 15:13:35 +0200

    Apple News API Using Providers

---
+++ src/providers/rest/rest.ts
@@ -0,0 +1,23 @@
+import { HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestProvider {
+
+  private apiUrl = 'https://newsapi.org/v2/everything?q=apple&from=2018-03-19&to=2018-03-19&sortBy=popularity&apiKey=c5ccf22ea7b1439b9da08405950cc4e5';
+
+  constructor(public http: HttpClient) {
+    console.log('Hello RestProvider Provider');
+  }
+
+  getActualities(){
+  	return this.http.get(this.apiUrl);
+  }
+
+}
